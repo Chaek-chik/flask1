c16114c24c81e32fc1f88ee9379485ec95f6e189
Vov <wow@vpanihin.ru>
2025-07-23 21:36:21 +0300

    Add Flask step One

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print('hello')
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Христос Воскресе!!!!!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
